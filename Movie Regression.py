bac8925deb378460eeb9ed7ec87216d93ce19f1a
fearghusson <53100380+fearghusson@users.noreply.github.com>
2021-01-21 13:30:24 -0500

    Update Movie Regression.py

--- Movie Regression.py
+++ Movie Regression.py
@@ -1,19 +1,14 @@
-'''superhero regression project 
-the idea is to see to what extent rating has on boxoffice from 2007 to 2019'''
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 17:18:38 2019
 
-import numpy as np
+@author: Fearghusson
+"""
 import pandas as pd
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
 import requests
+import matplotlib.pyplot as plt
 
-#url = r'http://www.omdbapi.com/?apikey=d42d26d9&t=Avengers'
-
-#r = requests.get(url)
-
-#print(r.json())
-
-def getmovie(title, year = None):
+def getmovie(movietitle, year = None):
     #this will get a list of elements from the json data
     try:
         #blank values that are used to fill out info
@@ -22,9 +17,9 @@ def getmovie(title, year = None):
    
         #if statement to determine if year is used
         if year == None:
-            url = 'http://www.omdbapi.com/?apikey=d42d26d9&t=' + str(title)
+            url = 'http://www.omdbapi.com/?apikey=d42d26d9&' + 't=' + movietitle
         else:
-            url =  'http://www.omdbapi.com/?apikey=d42d26d9&t=' + str(title) + '&y=' +str(year)
+            url =  'http://www.omdbapi.com/?apikey=d42d26d9&' + 't=' + movietitle + '&y=' +str(year)
    
         #requesting info from omdb and storing it to r
         r = requests.get(url)
@@ -53,15 +48,62 @@ def getmovie(title, year = None):
                 
         return listvalue
     
-    except (ValueError, KeyError) :
-       print('error - ' + title + ' may not be the correct title')
+    except ValueError:
+       print('error - ' + movietitle + ' may not be the correct title')
     
     return listvalue
 
+#blank lists to be filled out later to create each column of the dataframe
+title_list = []
+boxoffice_list = []
+rating = []
+year = []
 
+def createlists(listvalue):
+    try:
+        #appends the list values
+        title_list.append(listvalue[0])
+        boxoffice_list.append(listvalue[1])
+        rating.append(listvalue[2])
+        year.append(listvalue[3])
+    except IndexError:
+        print('missing value')
+        
+def fillout(allmovies, num = 10):
+    #this function loops over the all the movies and appends each of the blank lists
+    for i in range(num):
+        createlists(getmovie(allmovies[i]))
+        
+        
+#list of the actual movie titles to be searched
+movie_titles = []
+csv_movie = pd.read_csv(r"C:\Users\Fearghusson\Desktop\Movies.csv")
+titles_from_csv = csv_movie['title'].tolist()
 
-frame = {'Title':, '}
+for i in range(250):
+    movie_titles.append(titles_from_csv[i])
 
+#running the function to fill out all the series
+fillout(movie_titles, num = 100)
+#creating the series for 
+title_series = pd.Series(title_list)
+boxoffice_series = pd.Series(boxoffice_list)
+rating_series = pd.Series(rating)
+year_series = pd.Series(year)
+#create a dictionary for the dataframe
+frame = {'Title': title_series, 'BoxOffice':boxoffice_series, 'Rating': rating_series, 'Year':year_series }
+#here we remove the lines that have N/A from the df into a new df   
+df = pd.DataFrame(frame)
+df_filter_series = df['BoxOffice'] != 0
+df_final = df[df_filter_series]
 
+print(df_final)
 
+df_final.describe()
+
+df_final.plot(x='Rating', y='BoxOffice', style = 'o')
+plt.title('Rating to BoxOffice')
+plt.xlabel('Rating')
+plt.ylabel('BoxOffice')
+plt.show()
 
